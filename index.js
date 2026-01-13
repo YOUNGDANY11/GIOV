cda0fa29e883968819906dd24b294ff640ac8e66
Daniel Jose Morales Teatino <danielmoralesteatino2004@gmail.com>
2026-01-13 14:22:53 -0500

    Rama principal

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.listen(PORT,() =>{
+    console.log(`Server corriendo en el puerto ${PORT}`)
+})
